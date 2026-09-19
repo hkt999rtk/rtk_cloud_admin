310bd23c4ae439e82eb2f46deb87539f8b64095a
Kevin Huang <hkt999@gmail.com>
2026-09-19 17:00:54 +0800

    Show independent Cloud and Product CA readiness

--- internal/accountclient/client.go
+++ internal/accountclient/client.go
@@ -142,6 +142,9 @@ type User struct {
 }
 
 type Organization struct {
+	PKIStatus             string         `json:"pki_status"`
+	PKIOperationID        string         `json:"pki_operation_id"`
+	PKIIssuerID           string         `json:"pki_issuer_id,omitempty"`
 	ID                    string         `json:"id"`
 	Name                  string         `json:"name"`
 	TenantSlug            string         `json:"tenant_slug,omitempty"`
@@ -291,6 +294,9 @@ type FleetSummary struct {
 }
 
 type DeviceItemProfile struct {
+	PKIStatus          string         `json:"pki_status"`
+	PKIOperationID     string         `json:"pki_operation_id"`
+	PKIIssuerID        string         `json:"pki_issuer_id,omitempty"`
 	ID                 string         `json:"id"`
 	BrandCloudID       string         `json:"brand_cloud_id"`
 	ProfileKey         string         `json:"profile_key"`
